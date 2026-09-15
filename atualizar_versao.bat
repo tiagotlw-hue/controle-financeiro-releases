a2f3d0fd1111e13b33d44af1b653923c633ede39
unknown <tiago.tlw@gmail.com>
2026-09-15 01:16:31 -0300

    Limpa repositorio de releases: mantem apenas apk e versao.json

--- atualizar_versao.bat
+++
@@ -1,3 +0,0 @@
-﻿# powershell git config --global user.email "tiago.tlw@gmail.com"
-# powersehll git config --global user.name "tiagotlw-hue"
-powershell -ExecutionPolicy Bypass -File .\atualizar_versao.ps1
