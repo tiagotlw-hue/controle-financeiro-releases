24723d3ba038bcbb6ddb11a22656453ca07b3027
unknown <tiago.tlw@gmail.com>
2026-09-14 22:20:52 -0300

    Versao 0.0.8 (build 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.controlefinanceiro.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- atualizar_versao.bat
+++ atualizar_versao.bat
@@ -1,3 +1,3 @@
-﻿powershell git config --global user.email "tiago.tlw@gmail.com"
-powersehll git config --global user.name "tiagotlw-hue"
+﻿# powershell git config --global user.email "tiago.tlw@gmail.com"
+# powersehll git config --global user.name "tiagotlw-hue"
 powershell -ExecutionPolicy Bypass -File .\atualizar_versao.ps1
